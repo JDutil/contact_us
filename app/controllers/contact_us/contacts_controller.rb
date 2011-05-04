9ef78133a87cd2236b8804b95054d704633a3412
Jeff Dutil <jdutil21@gmail.com>
2011-05-04 01:36:02 -0400

    need to use namespace

--- app/controllers/contact_us/contacts_controller.rb
+++ app/controllers/contact_us/contacts_controller.rb
@@ -1,11 +1,11 @@
 class ContactUs::ContactsController < ApplicationController
   
   def new
-    @contact = Contact.new(:id=>1)
+    @contact = ContactUs::Contact.new(:id=>1)
   end
 
   def create
-    @contact = Contact.new(params[:contact_us_contact])
+    @contact = ContactUs::Contact.new(params[:contact_us_contact])
     
     if @contact.save    
       redirect_to('/', :notice => "Contact email was successfully sent.")
--- app/models/contact_us/contact.rb
+++ app/models/contact_us/contact.rb
@@ -21,7 +21,7 @@ class ContactUs::Contact
 
   def save
     if self.valid?
-      ContactMailer.contact_email(self).deliver
+      ContactUs::ContactMailer.contact_email(self).deliver
       return true
     end
     return false
