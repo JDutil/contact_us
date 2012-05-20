b19698cfd93d6ae56d889aa462261cd5b4f17205
Richard Huang <flyerhzm@gmail.com>
2012-05-20 10:12:52 +0800

    render new page for formtastic or simple_form

--- app/controllers/contact_us/contacts_controller.rb
+++ app/controllers/contact_us/contacts_controller.rb
@@ -7,12 +7,22 @@ class ContactUs::ContactsController < ApplicationController
       redirect_to('/', :notice => t('contact_us.notices.success'))
     else
       flash[:error] = t('contact_us.notices.error')
-      render :new
+      render_new_page
     end
   end
 
   def new
     @contact = ContactUs::Contact.new
+    render_new_page
   end
+
+  protected
+    def render_new_page
+      if "simple_form" == ContactUs.form_gem
+        render 'new_simple_form'
+      else
+        render "new_formtastic"
+      end
+    end
 
 end
