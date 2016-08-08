3c5e8f77761d026ac4407c7f61e12694e8d03212
briley <brian.riley@ucop.edu>
2016-08-08 08:15:07 -0700

    Removed trailing whitespaces that houndci was complaining about

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,15 @@ Rails.application.routes.draw do
       resources :contacts,
                 controller: "contact_us/contacts",
                 only:       [:new, :create]
-                
+
       get "contact-us" => "contact_us/contacts#new", as: :contact_us
     end
-    
+
   else
     resources :contacts,
               controller: "contact_us/contacts",
               only:       [:new, :create]
-              
+
     get "contact-us" => "contact_us/contacts#new", as: :contact_us
   end
 end
